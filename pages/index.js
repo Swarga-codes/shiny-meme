d65da601746d0f4a4811593e34ef7a23cbfb79bf
Aditya Singh <adibetu13@gmail.com>
2022-10-05 02:47:16 +0530

    meme 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Sidebar from '../components/Sidebar';
-
+import Feed from '../components/home/Feed';
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
 content: `max-w-[1400px] w-2/3 flex justify-between`
@@ -11,8 +11,8 @@ export default function Home() {
       <div className={style.content}>
 
 <Sidebar />
-<h2>Feed</h2>
-<h2>Widgets</h2>
+<Feed />
+  <h2>Widgetsr</h2>
 </div>
     </div>
   )
